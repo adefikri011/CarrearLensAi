8d8aff74c4784f3df97f72eed60191653d72f68e
Ade Fikri <fikriade257@gmail.com>
2026-05-12 10:56:21 +0700

    test3

--- app/page.tsx
+++ app/page.tsx
@@ -536,7 +536,7 @@ const Testimonials = () => {
             transition={{ duration: 0.6, delay: 0.1 }}
             className="text-gray-500 font-medium text-lg"
           >
-            Bergabunglah dengan mereka yang telah menemukan jalannya.
+            Bergabunglah dengan mereka yang telah menemukan jalannyaaaa.
           </motion.p>
         </div>
 
