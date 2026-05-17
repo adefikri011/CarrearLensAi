9f8385c51d62d994c7ac9b1d55bbfe8721cd9997
Ade Fikri Amirudin Amsyar <fikriade257@gmail.com>
2026-05-17 16:47:18 +0700

    feat: Add Supabase hostname to next.config.mjs
    Allows the Next.js application to load images from the Supabase storage bucket.

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,11 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'kbwibybypptujqyzhkmy.supabase.co',
+        pathname: '/**',
+      },
     ],
   },
 };
